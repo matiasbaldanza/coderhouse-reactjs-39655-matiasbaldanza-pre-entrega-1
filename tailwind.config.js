c78411dc8cdf9cb5a0e7599050f5f45a6404010e
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-02 01:37:23 -0300

    dejar proyecto en blanco, con tailwind configurado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: [require('daisyui')]
 }
-
